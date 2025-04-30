344be66d7ae15fb00404af63d4b2bcbcd24eeeb6
karolinakwasny <kwasnykarolina98@gmail.com>
2025-04-30 19:44:57 +0200

    5.7 done

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,41 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} ~ {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false)
+  const handleWhenHidden = { display: showDetails ? 'none' : '' }
+  const handleWhenVisible = { display: showDetails ? '' : 'none' }
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} ~ {blog.author}
+      <button
+        style={handleWhenHidden}
+        onClick={() => setShowDetails(true)}>
+          view
+      </button>
+      <button
+        style={handleWhenVisible}
+        onClick={() => setShowDetails(false)}>
+          hide
+      </button>
+      {showDetails && (
+        <div>
+          <div>{blog.url}</div>
+          <div>likes {blog.likes}
+          <button>like</button>
+          </div>
+          <div>{blog.user.name}</div>
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog
